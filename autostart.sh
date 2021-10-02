1553269f895f2cd1174fbf20556ffb3eea0bc930
liulei <liu6uu@163.com>
2021-10-02 21:02:04 +0800

    vpn change

--- autostart.sh
+++ autostart.sh
@@ -4,7 +4,8 @@ feh --bg-scale ~/Pictrue/background/5.jpg
 xsetroot -name qmarliu
 picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
 fcitx5 &
-/home/liul/Exe/squashfs-root/AppRun &
+# /home/liul/Exe/squashfs-root/AppRun &
+# expressvpn connect smart
 xfce4-power-manager &
 
 /bin/bash ~/scripts/wp-autochange.sh &
