b74b585e47628c587a794a2ef04945ef06fbbc8f
liulei <liu6uu@163.com>
2022-09-07 10:31:02 +0800

    clash在重启的时候会启动2个的问题

--- autostart.sh
+++ autostart.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
-#xsetroot -name qmarliu
+clash > ~/.config/clash/log.txt 2>&1 &
 picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
 fcitx5 &
-
-# /home/liul/Exe/squashfs-root/AppRun &
-# expressvpn connect smart
-#xfce4-power-manager &
-
 /bin/bash ~/scripts/wp-autochange.sh &
 /bin/bash ~/scripts/dwm-refresh-status.sh &
+/bin/bash ~/scripts/tap-to-click.sh &
+/bin/bash ~/scripts/inverse-scroll.sh &
 
-nohup clash > ~/.config/clash/log.txt 2>&1 &
-
-# /bin/bash ~/scripts/tap-to-click.sh
-# /bin/bash ~/scripts/inverse-scroll.sh
