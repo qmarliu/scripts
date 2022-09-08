978b79356e637266f60743f17157194b720da170
liulei <liu6uu@163.com>
2022-09-08 08:57:59 +0800

    add app-starter.sh

---
+++ app-starter.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+# 命令调用脚本
+
+set_vol() {
+    # case $1 in
+    #     up) pactl set-sink-volume 0 +5% ;;
+    #     down) pactl set-sink-volume 0 -5% ;;
+    case $1 in
+        up) amixer -D pulse sset Master 5%+ ;;
+        down) amixer -D pulse sset Master 5%- ;;
+    esac
+    ~/scripts/dwm-status-update.sh
+}
+
+case $1 in
+    filemanager) thunar ;;
+    alacritty) alacritty ;;
+    st) st ;;
+    rofi) rofi -show run ;;
+    rofi_window) rofi -show window -show-icons ;;
+    rofi_drun) rofi -show drun -show-icons ;;
+    keepass) keepass;;
+    xlock) xlock -mode puzzle;;
+    chrome) google-chrome-stable --proxy-server=socks5://localhost:1081 ;;
+    flameshot) flameshot gui -c -p ~/Pictures/screenshots ;;
+    open_last_screenshot) eog ~/Pictures/screenshots/$(ls -t ~/Pictures/screenshots | sed '2,9999d') >> /dev/null 2>&1 & ;;
+    set_vol) set_vol $2 ;;
+    change_wallpaper) ~/scripts/wp-change.sh ;;
+esac
