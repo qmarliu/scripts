f4ddba912c2e79f58bec9bde397170fbdb472156
liulei <liu6uu@163.com>
2022-09-18 22:42:37 +0800

    修改声音，添加内存，电量的显示

--- dwm-status-update.sh
+++ dwm-status-update.sh
@@ -11,34 +11,64 @@
 print_volume() {
 	# volume="$(amixer get Master | tail -n1 | sed -r 's/.*\[(.*)%\].*/\1/')"
 	volume="$(pactl get-sink-volume 0 | head -n1 | sed -r 's/.* (.*)% .*/\1/')"
-	if test "$volume" -gt 0
-	then
-		echo -e "Vol ${volume}"
-	else
-		echo -e "Mute"
+  if [ "$volume" -eq 0 ];
+  then echo -e "ﱝ";
+  else echo -e " ${volume}";
 	fi
 }
 
-get_battery_combined_percent() {
+print_bat() {
+  bat_text=$(acpi -b | sed 2d | awk '{print $4}' | grep -Eo "[0-9]+")
+  [ ! "$bat_text" ] && bat_text=$(acpi -b | sed 2d | awk '{print $5}' | grep -Eo "[0-9]+")
+  [ ! "$(acpi -b | grep 'Battery 0' | grep Discharging)" ] && charge_icon=""
+  if  [ "$bat_text" -ge 95 ]; then charge_icon=""; bat_icon="";
+  elif [ "$bat_text" -ge 90 ]; then bat_icon="";
+  elif [ "$bat_text" -ge 80 ]; then bat_icon="";
+  elif [ "$bat_text" -ge 70 ]; then bat_icon="";
+  elif [ "$bat_text" -ge 60 ]; then bat_icon="";
+  elif [ "$bat_text" -ge 50 ]; then bat_icon="";
+  elif [ "$bat_text" -ge 40 ]; then bat_icon="";
+  elif [ "$bat_text" -ge 30 ]; then bat_icon="";
+  elif [ "$bat_text" -ge 20 ]; then bat_icon="";
+  elif [ "$bat_text" -ge 10 ]; then bat_icon="";
+  else bat_icon=""; fi
 
-	# get charge of all batteries, combine them
-	total_charge=$(expr $(acpi -b | awk '{print $4}' | grep -Eo "[0-9]+" | paste -sd+ | bc));
-
-	# get amount of batteries in the device
-	battery_number=$(acpi -b | wc -l);
-
-	percent=$(expr $total_charge / $battery_number);
-
-	echo $percent;
+  if [[ -z $VAR ]]; then
+    echo "$charge_icon"
+    return
+  bat_icon=$charge_icon$bat_icon
+  echo "$bat_icon $bat_text%"
 }
 
-print_bat(){
-	echo "$(get_battery_combined_percent)%";
+print_mem() {
+  mem_total=$(cat /proc/meminfo | grep "MemTotal:"|awk '{print $2}')
+  mem_free=$(cat /proc/meminfo | grep "MemFree:"|awk '{print $2}')
+  mem_buffers=$(cat /proc/meminfo | grep "Buffers:"|awk '{print $2}')
+  mem_cached=$(cat /proc/meminfo | grep -w "Cached:"|awk '{print $2}')
+  men_usage_rate=$(((mem_total - mem_free - mem_buffers - mem_cached) * 100 / mem_total))
+  mem_icon=""
+  mem_text=$(echo $men_usage_rate | awk '{printf "%02d%", $1}')
+  echo "$mem_icon $mem_text"
 }
 
-print_date(){
-	# date '+%m月%d日 %H:%M %a 第%j天'
-	date '+%m/%d %H:%M %a'
+print_time() {
+  time_text="$(date '+%m/%d %H:%M %a')"
+  case "$(date '+%I')" in
+    "01") time_icon="" ;;
+    "02") time_icon="" ;;
+    "03") time_icon="" ;;
+    "04") time_icon="" ;;
+    "05") time_icon="" ;;
+    "06") time_icon="" ;;
+    "07") time_icon="" ;;
+    "08") time_icon="" ;;
+    "09") time_icon="" ;;
+    "10") time_icon="" ;;
+    "11") time_icon="" ;;
+    "12") time_icon="" ;;
+  esac
+
+  echo "$time_icon $time_text"
 }
 
-xsetroot -name "$(print_volume)$(print_date)"
+xsetroot -name "$(print_bat)$(print_mem)$(print_volume)$(print_time)"
